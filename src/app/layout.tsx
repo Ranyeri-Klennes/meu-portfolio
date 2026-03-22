890655afd012b177e1b4d8072e12397e35558c2f
Ranyeri Klennes <ranyeri_klennes@hotmail.com>
2026-03-22 16:19:40 -0300

    chore: add vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -24,6 +26,8 @@ export default function RootLayout({
           </filter>
         </svg>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
